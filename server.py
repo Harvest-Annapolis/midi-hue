d3f8b5e52e87477118ea2110e56ddc0fcf448a74
Sean Tyler Atwell <sta0003@auburn.edu>
2021-06-16 14:17:13 -0400

    Baseline actual function

--- server.py
+++ server.py
@@ -3,23 +3,39 @@ import time
 import rtmidi
 from hue_api import HueApi
 
+api = HueApi()
+do_the_hue(api)
+
 def join_the_hue():
     api = HueApi()
     api.create_new_user("10.1.0.99")
     api.save_api_key("the_hue_is_due.dat")
 
-def do_the_hue():
-    api = HueApi()
+def do_the_hue(api):
     api.load_existing("the_hue_is_due.dat")
     api.fetch_lights()
     api.fetch_groups()
-    [i.set_off() for i in api.groups[0].lights]
-    time.sleep(5)
-    [i.set_on() for i in api.groups[0].lights]
+    #[i.set_off() for i in api.groups[0].lights]
+    #time.sleep(5)
+    #[i.set_on() for i in api.groups[0].lights]
+
+def run_instruction(instructions):
+    for i in instructions:
+        group_name = i[1]
+        brightness = i[2]
+        if brightness = 0:
+            [i.set_off() for i in api.groups[0].lights]
+        else:
+            [i.set_brightness(brightness) for i in api.groups[0].lights]
+            [i.set_on() for i in api.groups[0].lights]
 
 def callback(message, time):
-    print(message)
-    print(time)
+    if message[2] != 0:
+        print(message)
+        with open("./instructions.txt", "r") as f:
+            instructions = [i.split(":") for i in f.readlines() if i[0] != '#']
+        if len([i for i in instructions if i[0] == message[1]]) > 0:
+            run_instructions([i for i in instructions if i[0] == str(message[1])])
 
 if __name__ == "__main__":
     midi_in = rtmidi.MidiIn()
@@ -27,13 +43,13 @@ if __name__ == "__main__":
     print(midi_in.get_ports())
     midi_in.open_port(0,name="Light Hub")
 
-
     print("Entering main loop. Press Control-C to exit.")
     try:
         # Just wait for keyboard interrupt,
         # everything else is handled via the input callback.
         while True:
-            time.sleep(1)
+            cmd=input("Run manual command>>>")
+            callback([144,cmd,127], 0)
     except KeyboardInterrupt:
         print('')
     finally:
