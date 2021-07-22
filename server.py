420ed751e26a520be8962200d4601063c5952ecf
SainTfactor <sta0003@auburn.edu>
2021-07-21 20:30:40 -0400

    Changes!!

--- server.py
+++ server.py
@@ -12,26 +12,25 @@ def do_the_hue(api):
     api.load_existing("the_hue_is_due.dat")
     api.fetch_lights()
     api.fetch_groups()
-    #[i.set_off() for i in api.groups[0].lights]
-    #time.sleep(5)
-    #[i.set_on() for i in api.groups[0].lights]
 
 def run_instructions(instructions):
+    global api
     for i in instructions:
         group_name = i[1]
-        brightness = i[2]
+        brightness = int(i[2].strip())
+        my_group = [j for j in api.groups if j.name == group_name][0]
         if brightness == 0:
-            [i.set_off() for i in api.groups[0].lights]
+            [j.set_off() for j in my_group.lights]
         else:
-            [i.set_brightness(brightness) for i in api.groups[0].lights]
-            [i.set_on() for i in api.groups[0].lights]
+            [j.set_on() for j in my_group.lights]
+            [j.set_brightness(brightness) for j in my_group.lights]
 
-def callback(message, time):
+def callback(data, question_mark):
+    message = data[0]
     if message[2] != 0:
-        print(message)
         with open("./instructions.txt", "r") as f:
             instructions = [i.split(":") for i in f.readlines() if i[0] != '#']
-        if len([i for i in instructions if i[0] == message[1]]) > 0:
+        if len([i for i in instructions if i[0] == str(message[1])]) > 0:
             run_instructions([i for i in instructions if i[0] == str(message[1])])
 
 api = HueApi()
@@ -41,7 +40,7 @@ if __name__ == "__main__":
     midi_in = rtmidi.MidiIn()
     midi_in.set_callback(callback)
     print(midi_in.get_ports())
-    midi_in.open_port(0,name="Light Hub")
+    midi_in.open_port(1,name="Light Hub")
 
     with open("./instructions.txt", "r") as f:
         instructions = f.read()
@@ -53,7 +52,7 @@ if __name__ == "__main__":
         # everything else is handled via the input callback.
         while True:
             cmd=input("Run manual command>>>")
-            callback([144,cmd,127], 0)
+            callback(([144,cmd,127], 0), "wat")
     except KeyboardInterrupt:
         print('')
     finally:
